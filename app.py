63ca4c0c0791820bb8767f9acc3a599640a7e2c1
ws wong <wswon@DESKTOP-TLTH8G6.localdomain>
2022-07-12 14:57:32 +0800

    css commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
+from waitress import serve
 
 
 app = Flask(__name__)
@@ -43,3 +44,8 @@ def show_post(post_id):
 def show_subpath(subpath):
     # show the subpath after /path/
     return 'Subpath %s' % escape(subpath)
+
+
+
+if __name__=="__main__":
+    serve(app, host='0.0.0.0', port=5001)
